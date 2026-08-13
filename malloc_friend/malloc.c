09bd01013dad7efc76aa23194fee1b4e9f85b846
csharpinthecsharp <trilllucien@gmail.com>
2026-08-13 16:09:39 +0200

    kf

--- malloc_friend/malloc.c
+++ malloc_friend/malloc.c
@@ -1,7 +1,72 @@
 #include "malloc.h"
 
-void*
-malloc(size_t size) {
+static t_zone* zone[3] = {NULL, NULL, NULL};
+
+t_block*
+create_block(size_t size, t_type type) {
+	t_block* block;
+	block = lookup_zone(size, type);
+	if (!block)
+		return (NULL);
+	return (block)
+}
+
+void
+lookup_zone(size_t size, t_type type) {
+	t_zone* zone;
+	zone = search_zone_space(size, type)
+	if (!zone) {
+		zone = create_zone(size, type)
+		if (!zone)
+			return (NULL);
+	}
+	block = carve(zone, size);
+	if (!block)
+		return (NULL);
+	return (block);
+}
+
+t_zone*
+search_zone_space(size_t size, t_type type) {
+	t_zone *head;
+	if (type == TINY)
+		head = zone[ZONE_TINY];
+	else if (type == SMALL)
+		head = zone[ZONE_SMALL];
+	else
+		head = zone[ZONE_LARGE];
+	size_t c;
+	while (head) {
+		if (zone_has_space(head, size) == True)
+			return (head);
+		head = head->next;
+	}
+	return (NULL);
+}
+
+t_bool
+zone_has_space(t_zone* zone, size_t size) {
+	t_block *block;
+	block = zone->blocks;
+	while (block) {
+		if (block->free && block->size >= size)
+			return (True);
+		block = block->next;
+	}
+	return (False);
+}
+
+t_zone*
+create_zone(size_t size, t_type type) {
+	return (NULL);
+}
+
+t_block* carve(t_zone zone, size_t size) {
+	return (NULL)
+}
+
+
+size_t alloc(size_t size) {
 	size_t map_size = size + sizeof(size_t);
 	map_size = (map_size + PAGE_SIZE - 1) / PAGE_SIZE;
 	map_size = map_size * PAGE_SIZE;
@@ -12,6 +77,21 @@ malloc(size_t size) {
 
 	*(size_t *)ptr = map_size;
 	return (ptr + sizeof(size_t));
+}
+
+void*
+malloc(size_t size) {
+	t_block* block;
+	if (size == 0)
+		return (NULL);
+	if (size <= TINY_MAX)
+		block = create_block(size, TINY);
+	else if (size <= SMALL)
+		block = create_block(size, SMALL);
+	else
+		block = create_block(size, LARGE);
+	if (!block)
+		return (NULL);
 }
 
 void
--- malloc_friend/malloc.h
+++ malloc_friend/malloc.h
@@ -12,19 +12,38 @@
 
 # define PAGE_SIZE (getpagesize())
 
-typedef enum s_ztype {
+# define TINY_MAX 400
+# define SMALL_MAX 800
+# define LARGE_MAX 1200
+
+# define ZONE_TINY 0
+# define ZONE_SMALL 1
+# define ZONE_LARGE 2
+
+typedef enum s_bool {
+    False,
+    True
+} t_bool;
+
+typedef enum s_type {
     TINY,
     SMALL,
     LARGE
-} t_ztype;
+} t_type;
 
 typedef struct s_block {
-
-    t_ztype type;
-    size_t msize;
-    struct s_zone* next;
+    size_t size;
+    int free;
+    struct s_block* next;
+    struct s_block* prev;
 } t_block;
 
+typedef struct s_zone {
+    t_type type;
+    size_t map_size
+    t_block *blocks;
+    struct s_zone *next;
+} t_zone;
 
 void* malloc(size_t size);
 void free(void* ptr);
